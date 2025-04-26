7ae0dd86c66479d48eb555960db1114095be7d3b
Ihimbru <kumbrianihimbru@gmail.com>
2025-04-26 18:29:45 +0100

    Started working on the story page

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -38,49 +38,166 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: Column(
-        spacing: 10,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 40.0),
-            child: Text(
-              'Explore Today\'s',
-              style: AppFont.black.copyWith(fontSize: 24),
-            ),
-          ),
-          SizedBox(
-            height: size.height * 0.15,
-            child: ListView.builder(
-              padding: const EdgeInsets.only(top: 5, left: 40),
-              scrollDirection: Axis.horizontal,
-              physics: const BouncingScrollPhysics(),
-              itemCount: 10,
-              itemBuilder: (context, index) {
-                return StoryWidget(
-                  index: index,
-                  isViewed: index % 2 == 0 ? true : false,
-                );
-              },
-            ),
-          ),
-          SizedBox(
-            height: size.height * 0.35,
-            child: CarouselView(
-              padding: const EdgeInsets.only(left: 40),
-              itemExtent: size.width * 0.65,
-              elevation: 0,
-              shrinkExtent: size.width * 0.2,
-              itemSnapping: true,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
+      body: SingleChildScrollView(
+        child: Column(
+          spacing: 10,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0),
+              child: Text(
+                'Explore Today\'s',
+                style: AppFont.black.copyWith(fontSize: 24),
               ),
-              children: List.generate(3, (index) {
-                return CarouselContainer(size: size, index: index);
-              }),
             ),
-          ),
-        ],
+            SizedBox(
+              height: size.height * 0.15,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(top: 5, left: 40),
+                scrollDirection: Axis.horizontal,
+                physics: const BouncingScrollPhysics(),
+                itemCount: 10,
+                itemBuilder: (context, index) {
+                  return StoryWidget(
+                    index: index,
+                    isViewed: index % 2 == 0 ? true : false,
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.35,
+              child: CarouselView(
+                padding: const EdgeInsets.only(left: 40),
+                itemExtent: size.width * 0.65,
+                elevation: 0,
+                shrinkExtent: size.width * 0.2,
+                itemSnapping: true,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                children: List.generate(3, (index) {
+                  return CarouselContainer(size: size, index: index);
+                }),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 40.0, right: 45, top: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Latest News',
+                    style: AppFont.black.copyWith(fontSize: 24),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+
+                    child: Text(
+                      'More',
+                      style: AppFont.roman.copyWith(
+                        fontSize: 24,
+                        color: AppColors.blue,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.25,
+              child: ListView.builder(
+                padding: const EdgeInsets.only(left: 40),
+                scrollDirection: Axis.vertical,
+                physics: const BouncingScrollPhysics(),
+                itemCount: 3,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      children: [
+                        Container(
+                          height: size.height * 0.15,
+                          width: size.width * 0.3,
+
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            image: DecorationImage(
+                              image: AssetImage('assets/images/carousel1.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 20),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Big Data'.toUpperCase(),
+                                style: AppFont.medium.copyWith(
+                                  fontSize: 18,
+                                  color: AppColors.blue,
+                                ),
+                              ),
+                              const SizedBox(height: 10),
+                              Text(
+                                'Why Big Data needs Thick Data',
+                                style: AppFont.roman.copyWith(fontSize: 16),
+                              ),
+                              const SizedBox(height: 30),
+                              Row(
+                                children: [
+                                  const Icon(
+                                    HugeIcons.strokeRoundedThumbsUp,
+                                    size: 18,
+                                    color: AppColors.darkBlue,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    '2.1k',
+                                    style: AppFont.roman.copyWith(
+                                      fontSize: 18,
+                                      color: AppColors.darkBlue,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  const Icon(
+                                    Icons.access_time_rounded,
+                                    size: 18,
+                                    color: AppColors.darkBlue,
+                                  ),
+                                  const SizedBox(width: 5),
+                                  Text(
+                                    '1hr ago',
+                                    style: AppFont.roman.copyWith(
+                                      fontSize: 18,
+                                      color: AppColors.darkBlue,
+                                    ),
+                                  ),
+                                  const SizedBox(width: 10),
+                                  IconButton(
+                                    onPressed: () {},
+                                    icon: Icon(
+                                      Icons.bookmark_rounded,
+                                      size: 18,
+                                    ),
+
+                                    color: AppColors.blue,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: FloatingActionButton(
--- lib/pages/story_screen.dart
+++ lib/pages/story_screen.dart
@@ -1,10 +1,110 @@
+import 'package:blogger/utils/app_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class StoryScreen extends StatelessWidget {
   const StoryScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Story Screen')));
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.blue,
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: size.height * 0.85,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/story.png'),
+
+                fit: BoxFit.cover,
+              ),
+              color: AppColors.darkBlueText,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            child: Stack(
+              children: [
+                Positioned(
+                  top: 20,
+                  left: 20,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_new_rounded),
+                    color: Colors.white,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  left: 20,
+                  child: Text(
+                    'The Story of\nThe World',
+                    style: AppFont.black.copyWith(
+                      color: Colors.white,
+                      fontSize: 30,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Column(
+                children: [
+                  Icon(HugeIcons.strokeRoundedArrowUp01, color: Colors.white),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 20,
+                        vertical: 10,
+                      ),
+                    ),
+                    child: Text(
+                      'Read More',
+                      style: AppFont.medium.copyWith(
+                        color: AppColors.darkBlueText,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              Column(
+                children: [
+                  Icon(
+                    Icons.favorite_rounded,
+                    color: Color(0xffFF3743),
+                    size: 35,
+                  ),
+
+                  Text(
+                    '450k',
+                    style: AppFont.roman.copyWith(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
